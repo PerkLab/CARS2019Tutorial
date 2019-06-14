273f9fe60d7098b0521467fd41b4df89a2d06742
Tamas Ungi <ungi.tamas@gmail.com>
2019-06-14 16:41:26 -0400

    Removing PyIGTLink and keras-vis from installer script, as they are not currently used in the tutorial.

--- Setup/setup_env_gpu.bat
+++ Setup/setup_env_gpu.bat
@@ -3,10 +3,7 @@
 
 if %1.==. goto No1
 
-set CurrentPath=%cd%
-set CurrentDrive=%CD:~0,2%
 set EnvironmentPath=%1
-set EnvironmentDrive=%~d1
 
 :: Create the environment and activate it
 
@@ -16,24 +13,6 @@ call activate %EnvironmentPath%
 :: Install additional packages
 
 call conda install -y keras pandas opencv==3.4.2 jupyter git scikit-learn matplotlib Pillow ipywidgets
-
-:: Install pyIGTLink from sourceopen
-
-call git clone -b pyIGTLink_client https://github.com/SlicerIGT/pyIGTLink.git %ProjectPath%\pyIGTLink
-call pip install -e %ProjectPath%\pyIGTLink
-
-:: Install keras-vis from source
-
-%EnvironmentDrive%
-cd %EnvironmentPath%
-
-call git clone https://github.com/raghakot/keras-vis.git %EnvironmentPath%\keras-vis
-cd keras-vis
-call python setup.py install
-
-%CurrentDrive%
-cd %CurrentPath%
-
 
 :: Exiting install script
 
